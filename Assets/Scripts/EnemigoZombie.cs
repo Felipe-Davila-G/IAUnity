d68f8f9beef3afbd81c57f4b925a38cac3f5882f
Felipe-Davila-G <jdavilag1@ucentral.edu.co>
2025-11-10 19:58:27 -0500

    Reapply "demo 2"
    This reverts commit 51c6e49915450b770cd81724435da9406d82609e.

--- Assets/Scripts/EnemigoZombie.cs
+++ Assets/Scripts/EnemigoZombie.cs
@@ -6,6 +6,7 @@ public class EnemigoZombie : Enemigo
 {
 
     private NavMeshAgent agente;
+    public Animator animaciones;
 
 
     void Awake()
@@ -17,6 +18,8 @@ public class EnemigoZombie : Enemigo
     public override void EstadoIdle()
     {
         base.EstadoIdle();
+        if (animaciones != null) animaciones.SetFloat("velocidad", 0);
+        if (animaciones != null) animaciones.SetBool("atacando", false);
         agente.SetDestination(transform.position);
 
 
@@ -25,12 +28,16 @@ public class EnemigoZombie : Enemigo
     public override void EstadoSeguir()
     {
         base.EstadoSeguir();
+        if (animaciones != null) animaciones.SetFloat("velocidad", 1);
+        if (animaciones != null) animaciones.SetBool("atacando", false);
         agente.SetDestination(target.position);
     }
 
     public override void EstadoAtacar()
     {
         base.EstadoAtacar();
+        if (animaciones != null) animaciones.SetFloat("velocidad", 0);
+        if (animaciones != null) animaciones.SetBool("atacando", true);
         agente.SetDestination(transform.position);
         transform.LookAt(target,Vector3.up);
     }
@@ -38,7 +45,13 @@ public class EnemigoZombie : Enemigo
     public override void EstadoMuerto()
     {
         base.EstadoMuerto();
-        
+        if (animaciones != null) animaciones.SetBool("vivo", false);
         agente.enabled = false;
+    }
+
+    [ContextMenu("Matar")]
+    public void Matar()
+    {
+        CambiarEstado(Estados.muerto);
     }
 }
